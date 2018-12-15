086c6d18de3b2f34fb7faccb7fecde9b257daa7a
Mike Durban <mikedurban.md@gmail.com>
2018-12-15 02:19:25 +0100

    First draft

---
+++ MissionIconHider.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         MissionIconHiding
+// @version      0.0.1
+// @description  Missionsicons ausblenden auf der Karte
+// @author       Glaeydar
+// @include      *://www.leitstellenspiel.de/
+// @grant        none
+// @namespace    https://github.com/Glaeydar/LSS_Scripts/MissionIconHider.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    /* Configuration */
+
+    /* Blendet die Icons anhand folgender Filter aus */
+    var filter = ['[Verband]', '[Event]'];
+
+    /* Configuration END */
+
+
+    /* DO NOT MODIFY UNLESS YOU KNOW WHAT YOU DO */
+    var tid = setInterval(mycode, 10000);
+
+    function mycode() {
+        var substringCount = filter.length;
+
+        try {
+            for (var i = mission_markers.length - 1; i >= 0; i--) {
+                var string = mission_markers[i]._tooltip._content;
+
+                if (filter.some(substring => string.includes(substring))) {
+                    map.removeLayer(mission_markers[i]);
+                }
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+})();
