7fd05ece790ffe9d0e0f2100068fa6753656e992
Mike Durban <mikedurban.md@gmail.com>
2018-12-29 11:34:29 +0100

    Unnötigen Code entfernt

--- ProtocollLinkInsert.user.js
+++ ProtocollLinkInsert.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ProtocollLinkInsert
-// @version      0.0.1
+// @version      0.0.2
 // @description  Fügt den Link zum Protokoll ins Verbandsmenü ein
 // @author       Glaeydar
 // @include      *://www.leitstellenspiel.de/
@@ -13,6 +13,5 @@
     $( document ).ready(function() {
         $('#alliance_li > ul > li:nth-child(10)')
             .before('<li role="presentation" class="alliance_true"><a href="/alliance_logfiles" class="lightbox-open">Protokoll</a></li>');
-        alert();
     });
 })();
