ffd027ad2268037835d83b72dfcac841f30331e4
Mike Durban <mikedurban.md@gmail.com>
2018-12-29 17:32:45 +0100

    Element werden zeitlich versetzt gelöscht
    Alte Elemente werden nun nach 30 Minunten (default) gelöscht.

--- DeleteObsoleteMissionObjects.user.js
+++ DeleteObsoleteMissionObjects.user.js
@@ -1,22 +1,42 @@
 // ==UserScript==
 // @name         DeleteObsoleteMissionObjects
-// @version      0.0.1
-// @description  Alte Missionsobjekte werden gelöscht !!! WICHTIG !!! Das Spielverhalten kann beeinträchtigt werden !!! WICHTIG !!!
+// @version      0.0.2
+// @description  Alte Missionsobjekte werden gelöscht
 // @author       Glaeydar
 // @include      *://www.leitstellenspiel.de/
 // @grant        none
-// @namespace    https://github.com/Glaeydar/LSS_Scripts/DeleteObsoleteMissionObjects.user.js
-// @namespace
+// @namespace    https://github.com/Glaeydar/LSS_Scripts/raw/master/DeleteObsoleteMissionObjects.user.js
+// @updateURL    https://github.com/Glaeydar/LSS_Scripts/raw/master/DeleteObsoleteMissionObjects.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
+
+    /*
+    Anzahl der Minuten, nach der ein ausgeblendetes Element gelöscht wird.
+    Dieser Wert sollte nicht kleiner als 15 sein, da es sonst zu Spielfehlern kommen könnte.
+    Default: 30
+    */
+    var minutes = 30;
+
+    var DEBUG = false;
+
+    /* DO NOT MODIFY UNLESS YOU KNOW WHAT YOU DO */
     var tid = setInterval(mycode, 5000);
 
     function mycode() {
         $('#mission_list > div').each(function () {
+            /* Remove the element if it is not displayed and has a time attribute */
             if($(this).css("display") == "none"){
-                $(this).remove();
+                if(!$(this).attr('time')) {
+                    $(this).attr('time', Math.floor($.now()/1000));
+                } else {
+                    /* Check if the element is older than 30 minutes */
+                    if((Math.floor($.now()/1000)-(parseInt($(this).attr('time')))) > (minutes * 60)) {
+                        if(DEBUG) {console.log("[DeleteObsoleteMissionObjects] Removed element " + $(this).attr('id') + " after " + (Math.floor($.now()/1000) - ($(this).attr('time')) + "seconds"));}
+                        $(this).remove();
+                    }
+                }
             }
         });
     }
