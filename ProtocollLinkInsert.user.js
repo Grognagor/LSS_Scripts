0bb7f6814fc0390cd5d19934383b3803bbaa8e55
Mike Durban <mikedurban.md@gmail.com>
2019-01-01 18:25:42 +0100

    Leitstellenansicht ebenfalls verlinkt

--- ProtocollLinkInsert.user.js
+++ ProtocollLinkInsert.user.js
@@ -13,5 +13,9 @@
     $( document ).ready(function() {
         $('#alliance_li > ul > li:nth-child(10)')
             .before('<li role="presentation" class="alliance_true"><a href="/alliance_logfiles" class="lightbox-open">Protokoll</a></li>');
+
+        $('#navbar-main-collapse > ul > li:nth-child(4) > ul > li:nth-child(10)')
+            .before('<li role="presentation" class="alliance_true"><a href="/leitstellenansicht" class="lightbox-open">Leitstellenansicht</a></li>');
+
     });
 })();
